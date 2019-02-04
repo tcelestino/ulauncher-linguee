0e09516b7be03a673c023ccc601ceeb433adbb3b
Tiago Celestino <tiago.farias@catho.com>
2019-02-04 17:08:57 -0200

    feat: init project

---
+++ README.md
@@ -0,0 +1,28 @@
+# ulauncher-linguee
+
+> Simple ULauncher extensions that allows you to find word defintion using Linguee.com.
+
+## Demo
+
+[Create gif]
+
+## Features
+
+You can find word defintions on Linguee using `linguee <query>` and it opens the browser when you hit enter. At moment you can only find to defintions in:
+
+  - english -> brazilian portugueses
+  - brazilian portuguese -> english.
+
+I'm working to show the others languages option.
+
+## Installation
+
+Open ulauncher options and go to `Extensions` > `Add extension` and fill in `https://github.com/tcelestino/ulauncher-linguee`.
+
+## TODO
+
+Look [here](https://github.com/tcelestino/ulauncher-linguee/projects/1)
+
+## License
+
+The MIT License (MIT)
---
+++ main.py
@@ -0,0 +1,44 @@
+import urllib
+import logging
+
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+
+LOGGER = logging.getLogger(__name__)
+
+def urlencode(q):
+    return urllib.urlencode(q)
+
+class LingueeExtension(Extension):
+
+    def __init__(self):
+        super(LingueeExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = list()
+
+        if event.get_argument():
+            LOGGER.info('Word Linguee search for "{}"'.format(event.get_argument()))
+            items.append(
+                ExtensionResultItem(
+                    icon='images/icon.png',
+                    name='Define words on Linguee',
+                    description='Define word "{}".'.format(event.get_argument()),
+                    on_enter=OpenUrlAction(
+                        'https://www.linguee.com/english-portuguese/search?{}'.format(urlencode({ 'source': 'auto', 'query': event.get_argument() }))
+                    )
+                )
+            )
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    LingueeExtension().run()
